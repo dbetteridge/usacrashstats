bb3affe3262414328d235f4fe12ab55fa8356f4a
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2016-10-08 20:59:42 +0800

    remove console log

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -14,7 +14,7 @@ function displayData(data){
 
 function countData(data){
     var countAges = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0};
-    console.log(data[data.length - 1]);
+   
     data.map(function(element){
         
         countAges[element.A_AGE3] +=1
